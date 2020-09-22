65a49ff26f535f024441ef45753ea66a40ada484
sullis <github@seansullivan.com>
2020-09-21 21:48:36 -0700

    add FutureConverters (#1)

---
+++ src/main/scala/org/gfccollective/vavr/future/FutureConverters.scala
@@ -0,0 +1,73 @@
+package org.gfccollective.vavr.future
+
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+
+import io.vavr.concurrent.{Future => VavrFuture}
+import io.vavr.control.{Try => VavrTry}
+import org.gfccollective.vavr.VavrConverters.VavrTryConverter
+
+import scala.concurrent.{CanAwait, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.util.Try
+
+/**
+ * In spirit of scala.collection.JavaConverters.
+ *
+ * Implicit conversion functions for VAVR Future objects.
+ */
+object FutureConverters {
+
+  implicit class FutureConverter[T](val vFuture: VavrFuture[T]) extends AnyVal {
+    @inline def asScala: Future[T] = {
+      vFuture match {
+        case adapter: ScalaFutureAdapter[T] => adapter.getScalaFuture
+        case _ => new VavrFutureAdapter[T](vFuture)
+      }
+    }
+  }
+
+  implicit class VavrFutureConverter[T](val sFuture: Future[T]) extends AnyVal {
+    @inline def asJava: VavrFuture[T] = new ScalaFutureAdapter(sFuture)
+  }
+
+  def registerOnComplete[T, U >: VavrTry[T]](scalaFuture: Future[T], action: Consumer[U]): Unit = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    scalaFuture.onComplete(result => {
+      action.accept(result.asJava)
+    })
+  }
+
+  case class VavrFutureAdapter[T](vavrFuture: VavrFuture[T]) extends Future[T] {
+
+    override def onComplete[U](f: Try[T] => U)(implicit executor: ExecutionContext): Unit = {
+      vavrFuture.onComplete(new Consumer[VavrTry[T]]() {
+        override def accept(result: VavrTry[T]): Unit = {
+          import org.gfccollective.vavr.VavrConverters._
+          f.apply(result.asScala)
+        }
+      })
+    }
+
+    override def isCompleted: Boolean = vavrFuture.isCompleted
+
+    override def value: Option[Try[T]] = {
+      import org.gfccollective.vavr.VavrConverters._
+      vavrFuture.getValue.asScala.map(_.asScala)
+    }
+
+    override def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext): Future[S] = ??? // TODO
+
+    override def transformWith[S](f: Try[T] => Future[S])(implicit executor: ExecutionContext): Future[S] = ??? // TODO
+
+    override def ready(atMost: Duration)(implicit permit: CanAwait): VavrFutureAdapter.this.type = {
+      vavrFuture.await(atMost.toMillis, TimeUnit.MILLISECONDS)
+      this
+    }
+
+    override def result(atMost: Duration)(implicit permit: CanAwait): T = {
+      vavrFuture.await(atMost.toMillis, TimeUnit.MILLISECONDS).get
+    }
+  }
+}
---
+++ src/test/scala/org/gfccollective/vavr/future/FutureConvertersTest.scala
@@ -0,0 +1,115 @@
+package org.gfccollective.vavr.future
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Consumer
+
+import io.vavr.CheckedFunction0
+import io.vavr.concurrent.{Future => VavrFuture}
+import io.vavr.control.{Try => VavrTry}
+
+import scala.concurrent.{Await, Future}
+import scala.util.Success
+import org.scalatest.concurrent.Eventually
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.{Millis, Seconds, Span}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+
+class FutureConvertersTest
+  extends AnyFunSuite
+  with Matchers
+  with Eventually
+  with ScalaFutures {
+
+  implicit override val patienceConfig = PatienceConfig(
+                                             timeout = scaled(Span(1, Seconds)),
+                                             interval = scaled(Span(5, Millis)))
+
+  test("Vavr Future asScala") {
+    import FutureConverters._
+
+    val sFuture: Future[String] = makeVavrFuture.asScala
+
+    val counter = new AtomicInteger(0)
+
+    sFuture.onComplete { result =>
+      counter.incrementAndGet()
+    }
+
+    eventually {
+      counter.intValue should be(1)
+      sFuture.isCompleted should be(true)
+      sFuture.futureValue should be("bonjour")
+      sFuture.value should be(Some(Success("bonjour")))
+      Await.result(sFuture, Duration(5, MILLISECONDS)) should be("bonjour")
+    }
+  }
+
+  test("Scala Future asJava") {
+    import FutureConverters._
+
+    val vFuture: VavrFuture[String] = makeScalaFuture.asJava
+
+    val counter = new AtomicInteger(0)
+
+    vFuture.onComplete(new Consumer[VavrTry[String]]() {
+      override def accept(t: VavrTry[String]): Unit = {
+        counter.incrementAndGet()
+      }
+    })
+
+    eventually {
+      counter.intValue should be(1)
+      vFuture.isCompleted should be(true)
+      vFuture.isSuccess should be(true)
+      vFuture.isFailure should be(false)
+      vFuture.get should be("bonjour")
+      vFuture.await.get should be("bonjour")
+      vFuture.await(2, TimeUnit.SECONDS).get should be("bonjour")
+      vFuture.getOrElse("zzz") should be("bonjour")
+      vFuture.getOrNull should be("bonjour")
+    }
+  }
+
+  test("roundtrip") {
+    import FutureConverters._
+
+    val vFuture: VavrFuture[String] = makeScalaFuture.asJava
+
+    eventually {
+      vFuture.isCompleted should be(true)
+      vFuture.isSuccess should be(true)
+      vFuture.isFailure should be(false)
+      vFuture.get should be("bonjour")
+
+      val sFuture = vFuture.asScala
+      sFuture.isCompleted should be(true)
+      sFuture.futureValue should be("bonjour")
+      Await.result(sFuture, Duration(5, MILLISECONDS)) should be("bonjour")
+    }
+  }
+
+  private def makeScalaFuture: Future[String] = {
+    Future {
+      Thread.sleep(200L)
+      "bonjour"
+    }
+  }
+
+  private def makeVavrFuture: VavrFuture[String] = {
+    // the Scala compiler was having trouble invoking
+    // VavrFuture.of because 'of' is an overloaded method.
+    // My workaround is to explicitly create a CheckedFunction0 object.
+    val computation = new CheckedFunction0[String]() {
+      override def apply(): String = {
+        Thread.sleep(200L)
+        "bonjour"
+      }
+    }
+    VavrFuture.of(computation)
+  }
+}
