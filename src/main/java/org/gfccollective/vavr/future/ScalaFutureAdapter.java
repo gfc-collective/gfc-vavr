5a2a653dd2451d2f548c03cf6f8262c3b8b5755c
Sean C. Sullivan <github@seansullivan.com>
2020-09-22 11:37:29 -0700

    format Java source files

--- src/main/java/org/gfccollective/vavr/future/ScalaFutureAdapter.java
+++ src/main/java/org/gfccollective/vavr/future/ScalaFutureAdapter.java
@@ -15,78 +15,76 @@ import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 /**
- * ScalaFutureAdapter is implemented in Java because it
- * is impossible to implement in Scala. The Scala compiler
- * does not allow us to subclass io.vavr.concurrent.Future.
- * Vavr's Future implements a method called 'eq' and this
- * makes the Scala compiler very unhappy.
+ * ScalaFutureAdapter is implemented in Java because it is impossible to implement in Scala. The
+ * Scala compiler does not allow us to subclass io.vavr.concurrent.Future. Vavr's Future implements
+ * a method called 'eq' and this makes the Scala compiler very unhappy.
  */
-public class ScalaFutureAdapter<T>
-    implements io.vavr.concurrent.Future<T> {
+public class ScalaFutureAdapter<T> implements io.vavr.concurrent.Future<T> {
 
-    private final Future<T> scalaFuture;
+  private final Future<T> scalaFuture;
 
-    public ScalaFutureAdapter(Future<T> scalaFuture) {
-        this.scalaFuture = scalaFuture;
-    }
+  public ScalaFutureAdapter(Future<T> scalaFuture) {
+    this.scalaFuture = scalaFuture;
+  }
 
-    protected Future<T> getScalaFuture() {
-        return scalaFuture;
-    }
+  protected Future<T> getScalaFuture() {
+    return scalaFuture;
+  }
 
-    @Override
-    public io.vavr.concurrent.Future<T> await() {
-        return await(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-    }
+  @Override
+  public io.vavr.concurrent.Future<T> await() {
+    return await(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+  }
 
-    @Override
-    public io.vavr.concurrent.Future<T> await(final long timeout, final TimeUnit unit) {
-        if (!scalaFuture.isCompleted()) {
-            try {
-                Await.result(scalaFuture, Duration.apply(timeout, unit));
-                return this;
-            } catch (InterruptedException e) {
-                return io.vavr.concurrent.Future.failed(e);
-            } catch (TimeoutException e) {
-                return io.vavr.concurrent.Future.failed(e);
-            }
-        }
+  @Override
+  public io.vavr.concurrent.Future<T> await(final long timeout, final TimeUnit unit) {
+    if (!scalaFuture.isCompleted()) {
+      try {
+        Await.result(scalaFuture, Duration.apply(timeout, unit));
         return this;
+      } catch (InterruptedException e) {
+        return io.vavr.concurrent.Future.failed(e);
+      } catch (TimeoutException e) {
+        return io.vavr.concurrent.Future.failed(e);
+      }
     }
+    return this;
+  }
 
-    @Override
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        return false;
-    }
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    return false;
+  }
 
-    @Override
-    public ExecutorService executorService() throws UnsupportedOperationException {
-        return DEFAULT_EXECUTOR_SERVICE;
-    }
+  @Override
+  public ExecutorService executorService() throws UnsupportedOperationException {
+    return DEFAULT_EXECUTOR_SERVICE;
+  }
 
-    @Override
-    public Option<Try<T>> getValue() {
-        scala.Option<scala.util.Try<T>> scalaValue = scalaFuture.value();
-        VavrOptionConverter optionConverter = new VavrOptionConverter(scalaValue);
-        Option<Try<T>> vavrOption = optionConverter.asVavrOption().map(
-                (value) -> new VavrTryConverter((scala.util.Try<T>) value).asVavrTry()
-        );
-        return vavrOption;
-    }
+  @Override
+  public Option<Try<T>> getValue() {
+    scala.Option<scala.util.Try<T>> scalaValue = scalaFuture.value();
+    VavrOptionConverter optionConverter = new VavrOptionConverter(scalaValue);
+    Option<Try<T>> vavrOption =
+        optionConverter
+            .asVavrOption()
+            .map((value) -> new VavrTryConverter((scala.util.Try<T>) value).asVavrTry());
+    return vavrOption;
+  }
 
-    @Override
-    public boolean isCancelled() {
-        return false;
-    }
+  @Override
+  public boolean isCancelled() {
+    return false;
+  }
 
-    @Override
-    public boolean isCompleted() {
-        return scalaFuture.isCompleted();
-    }
+  @Override
+  public boolean isCompleted() {
+    return scalaFuture.isCompleted();
+  }
 
-    @Override
-    public io.vavr.concurrent.Future<T> onComplete(Consumer<? super Try<T>> action) {
-        FutureConverters.registerOnComplete(scalaFuture, action);
-        return this;
-    }
+  @Override
+  public io.vavr.concurrent.Future<T> onComplete(Consumer<? super Try<T>> action) {
+    FutureConverters.registerOnComplete(scalaFuture, action);
+    return this;
+  }
 }
